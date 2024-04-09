c79be924be2771bcda5501e0eea632b27d03920a
jan.matoniak <jan.matoniak@slovenskoit.sk>
2024-04-09 15:10:20 +0200

    Initialization of project

---
+++ WBPO-Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WBPO-Assignment
+//
+//  Created by jan.matoniak on 09/04/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
